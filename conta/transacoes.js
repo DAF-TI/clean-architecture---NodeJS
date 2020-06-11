b87d01ded9ae8c93a6a2dcded68175391bcd47c3
ALBERTO FREITAS CASTELLO BRANCO <45492741+AlbertoCastelloBranco@users.noreply.github.com>
2020-06-11 14:37:26 -0300

    .

--- conta/transacoes.js
+++ conta/transacoes.js
@@ -3,7 +3,7 @@ export default function buildMakeTransacao () {
     return function makeTransacao ({ idTransacao, idConta, valor, dataTransacao } = {}) {
 
       if ( valor < 0) {
-        throw new Error('Valor não pode ser negativo.')
+        throw new Error('Valor da transação não pode ser negativo.')
       }     
       return Object.freeze({
         getIdConta: () => idConta,
